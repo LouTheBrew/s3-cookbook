f1a9875c6e64cc4aead15daa07ec92a330cfceb8
ldesiqueira <eldusfelipus@gmail.com>
2016-07-08 11:18:29 -0700

    commit for version 0.1.22

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'louthebrew@gmail.com'
 license 'all_rights'
 description 'Provides an s3 resource interface'
 long_description 'Provides an s3 resource interface'
-version '0.1.0'
+version '0.1.22'
 
 depends 'poise'
